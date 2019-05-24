967c3a72c3b94955e0f6096d95b52bea9fdabac0
antonellam <antonella.michelli@gmail.com>
2019-05-24 19:09:07 +0200

    Build docs folder

---
+++ docs/precache-manifest.40f55afc551ce29581acfa6176283a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a6933fca5629fb3cf41430965b38fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5810d5714d435e63b3f",
+    "url": "./static/css/main.b09dfa04.chunk.css"
+  },
+  {
+    "revision": "0b8c076f158cac11e7a5",
+    "url": "./static/js/2.e8bb2bda.chunk.js"
+  },
+  {
+    "revision": "d5810d5714d435e63b3f",
+    "url": "./static/js/main.6edeea10.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
